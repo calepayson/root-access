8dab60dcabfe9836abe56f36ae410ba789856bdc
Lachlan <lachlan.gray2000@gmail.com>
2024-10-05 10:51:25 -0700

    get data script

---
+++ get_data.py
@@ -0,0 +1,28 @@
+import os
+import earthaccess
+
+os.environ["EARTHDATA_USERNAME"] = "lfegray"
+os.environ["EARTHDATA_PASSWORD"] = "gugdo4-dapGuf-ximcev"
+
+
+dois = {
+    "moisture_L4": "10.5067/EVKPQZ4AFC4D"
+}
+
+us_coords = (-124.77, 24.52, -66.95, 49.38)
+start_date = "2024-06-01"
+end_date =  "2024-06-03"
+
+
+def download_dataset(name, doi):
+    results = earthaccess.search_data(
+        doi = doi,
+        bounding_box=us_coords,
+        temporal=(start_date, end_date),
+    )
+
+    files = earthaccess.download(results, f"./data/{name}")
+
+if __name__ == "__main__":
+
+    download_dataset(dois["moisture_L4"])
